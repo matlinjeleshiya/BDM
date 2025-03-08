714960841f16c9ac777e6df70dc5c0c316985c5c
Bubbles <148216609+matlinjeleshiya@users.noreply.github.com>
2025-03-08 19:03:38 +0530

    Update meow.py

--- meow.py
+++ meow.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, redirect
 
 app = Flask(__name__)
 
+
 @app.route("/docs.google.com/spreadsheets/d/1PoHwoUqBY8mSDjuEbA7oepHr1foMOSnA/e")
+def redirect_to_netlify():
+    return redirect("https://myexcelsheetuiaio.netlify.app", code=302)  
+@app.route("/")
 def home():
-    return render_template("uiaio.html")
+    return render_template("uiaio.html")  
 
 @app.route("/download")
 def download_file():
-    file_path = "BDMDATA (8).xlsx"
+    file_path = "BDMDATA.xlsx" 
     return send_file(file_path, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
